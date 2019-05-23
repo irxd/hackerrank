4f5142b33a1119b9e88c0ec4b570793c92607b22
Irsyad <irsyart@gmail.com>
2019-05-24 00:11:31 +0700

    Add solution

---
+++ counting-valleys.js
@@ -0,0 +1,23 @@
+# https://www.hackerrank.com/challenges/counting-valleys/problem
+
+function countingValleys(n, s) {
+    let altitude = 0;
+    let startCounter = false;
+    let valleyCounter = 0;
+
+    for (let i = 0; i <= n; i++) {
+        if (s[i] === 'D') {
+            altitude--;
+        } else {
+            altitude++;
+        }
+
+        if (altitude === -1) startCounter = true; // valley start, go below sea level
+        if (altitude === 0 && startCounter) { // valley end, go up to sea level
+            valleyCounter++;
+            startCounter = false;
+        }
+    }
+
+    return valleyCounter;
+}
